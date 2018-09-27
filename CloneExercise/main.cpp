44a4dc7c463a9a5f7caf6b7dc10515fdeedfadb8
Raymond <raymonl4963@student.vvc.edu>
2018-09-27 15:08:06 -0700

    First Commit

---
+++ CloneExercise/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * File: hello.cpp
+ * Developer: Raymond Laughrey
+ * Email: raymonl4963@student.vvc.edu
+ * Description : This program will output the answer to life, the universe, and everything.
+**/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int answer = 84 / 2; // Calculate the answer
+    cout << "Hello World" << endl;
+    cout << "The answer to life, the universe, and everything is: " << answer<< endl;
+    return 0;
+}
