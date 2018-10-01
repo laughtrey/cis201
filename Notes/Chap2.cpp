e3298b1ecb310ce90b200a99ec0b3b9c044c5d8e
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-09-30 22:37:13 -0700

    updated chapter 2 notes

--- Notes/Chap2.cpp
+++ Notes/Chap2.cpp
@@ -9,10 +9,24 @@ int main()
   cout << "or you can simply type 'variable++' and it will add one to the variable" << endl;
   cout << "type variable-- and it will subtract one" << endl;
   enum weekday { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY }; //declaring multiple types of a variable
-  cout << "" << endl;
 
 //-------
 
   cout << "The '%' operator gives the remainder of an integer, the / operator divides" << endl;
+  cout << "square root is written sqrt(x)" << endl;
+  cout << "to computer exponants, write pow(x, n)" << endl;
+  cout << "to use sqrt and pow you must include <cmath>" << endl;
+  cout << "(-b + sqrt(b * b - 4 * a *c)) / (2 * a)" << endl; //Quadratic Formula
+  cout << "strings are like inputs for words" << endl;
+  cout << "getline (cin, name); gets the entire line of input and assigns it the name value" << endl;
+  cout << "once you have a string you can extract substrings with s.substr(start, length)" << endl;
+  //string greeting = "Hello, World!\n";
+  //string sub = greeting.substr(0, 4); //sub is Hell
+  cout << "" << endl;
+  cout << "" << endl;
+  cout << "" << endl;
+  cout << "" << endl;
+  cout << "" << endl;
+  cout << "" << endl;
   return 0;
 }
