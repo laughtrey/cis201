4557755cd8cd9ca36ecce48682e8a6f1f9f3609e
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-10-06 16:31:00 -0700

    Chapter 5 notes and remoteftp settings

---
+++ Notes/Chap5.cpp
@@ -0,0 +1,29 @@
+//Chapter 5 notes Big C++
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+//ftp-remote
+{
+string remainder; //Read remainder of line
+getline(cin, remainder);
+//Now you are ready to call getline again
+
+//Class definition
+/**
+class ClassName
+{
+public:
+   constructor declarations
+   member function declarations
+private:
+   data fields
+};
+---Define the interface and data fields of a class---
+
+Class definitions always end in };
+*/
+
+
+   return 0;
+}
