70ee041c6ecb3cf5e17f0d9792c4208a6c9898a8
Raymond <raymonl4963@student.vvc.edu>
2018-10-11 11:27:36 -0700

    Thursday work in ValidNumber

--- ValidNumber/main.cpp
+++ ValidNumber/main.cpp
@@ -22,20 +22,23 @@ int to_int(char c)
 }
 bool is_valid_number(string s)
 {
+  for(double i = (is_digit(s.at(0)) ); 0 < (is_digit(s.at(9999)) ); i++)
+
+  return true;
 }
 int main()
 {
    string s = "30349875340958034985039485093485024a958";
    if(is_valid_number(s))
    {
-      cout << "yes it is valid";
+      cout << "yes it is valid\n";
       //cout << s.at(0) << endl;
       //cout << s.at(1) << endl;
       //cout << s.at(2) << endl;
    }
    else
    {
-      cout << "no it is not valid";
+      cout << "no it is not valid\n";
    }
    return 0;
 }
