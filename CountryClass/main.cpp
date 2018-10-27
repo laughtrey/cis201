09e5da0653ad51a49908325eb5ea8a07f1ee3490
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-10-26 22:10:11 -0700

    Ihavenoideawhatimdoing.jpg

--- CountryClass/main.cpp
+++ CountryClass/main.cpp
@@ -17,24 +17,60 @@ using namespace std;
 class Country
 {
 private:
-   double country_population,country_area;
+   double population, area, density;
+   string name;
 public:
    Country(); // Constructor
-   Country(double area,double population); // Constructor with parameters
+   Country(double area, double population); // Constructor with parameters
+   void read(); // Mutator
+   bool is_larger(Country b); // Compares the two countries and finds the larger area
+   bool is_more_populated(Country b); // Compares the two countries and finds the more popuated
+   bool is_denser(Country b); // Compares the two countries and finds the denser populated
+   void print() const; // Prints the results
+   /*
    void set_population(double population); // Mutator
    void set_area(double area); // Mutator
-   bool is_larger(Country b); 
-   bool is_denser(Country b);
-   bool is_more_populated(Country b);
+   */
 };
 // Constructor
-Country::Country(double area,double population)
+Country::Country(double area, double population)
 {
-   area = 1.0;
-   population = 0.0;
+   area = 1;
+   population = 0;
 }
 // ---Functions---
 // Mutator or setter functions
+void Country::read()
+{
+   cout << "Please enter the name of the country: ";
+   getline(cin, name);
+   cout << "Please enter the population: ";
+   cin >> population;
+   cout << "Please enter the area in miles squared: ";
+   cin >> area;
+}
+bool Country::is_larger(Country b)
+{
+   if (area == 0) return true;
+   if (b.area == 0) return false;
+   return area > b.area;
+}
+bool Country::is_more_populated(Country b)
+{
+
+}
+bool Country::is_denser(Country b)
+{
+
+}
+void Country::print() const
+{
+   cout << name
+      << "Population: " << population
+      << "Area: "       << area
+      << "Density: "    << density;
+}
+/*
 void Country::set_population(double population)
 {
    country_population = population;
@@ -43,9 +79,10 @@ void Country::set_area(double area)
 {
    country_area = area;
 }
-
+*/
 int main()
 {
-
+   Country CountryA();
+   Country CountryB();
    return 0;
 }
