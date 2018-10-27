6a2e9186f60ddbbdf604b146a033adb6ec6cfb58
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-10-27 15:24:22 -0700

    I still have no idea what im doin

--- CountryClass/main.cpp
+++ CountryClass/main.cpp
@@ -21,11 +21,11 @@ private:
    string name;
 public:
    Country(); // Constructor
-   Country(int area, int population); // Constructor with parameters
+   //Country(int area, int population); // Constructor with parameters
    void read(); // Mutator
-   bool is_larger(Country b); // Compares the two countries and finds the larger area
-   bool is_more_populated(Country b); // Compares the two countries and finds the more popuated
-   bool is_denser(Country b); // Compares the two countries and finds the denser populated
+   bool is_larger_than(Country b) const; // Compares the two countries and finds the larger area
+   bool is_more_populated_than(Country b) const; // Compares the two countries and finds the more popuated
+   bool is_denser_than(Country b) const; // Compares the two countries and finds the denser populated
    void print() const; // Prints the results
    /*
    void set_population(double population); // Mutator
@@ -33,7 +33,7 @@ public:
    */
 };
 // Constructor
-Country::Country(int area, int population)
+Country::Country()
 {
    area = 1;
    population = 0;
@@ -49,19 +49,23 @@ void Country::read()
    cout << "Please enter the area in kilometers squared: ";
    cin >> area;
 }
-bool Country::is_larger(Country b)
+bool Country::is_larger_than(Country largest) const
 {
    if (area == 0) return true;
-   if (b.area == 0) return false;
-   return area > b.area;
+   if (largest.area == 0) return false;
+   return area > largest.area;
 }
-bool Country::is_more_populated(Country b)
+bool Country::is_more_populated_than(Country populous) const
 {
-
+   if (population == 0) return true;
+   if (populous.population == 0) return false;
+   return population > populous.population;
 }
-bool Country::is_denser(Country b)
+bool Country::is_denser_than(Country densest) const
 {
-
+   if (density == 0) return true;
+   if (densest.density == 0) return false;
+   return density > densest.density;
 }
 void Country::print() const
 {
@@ -70,19 +74,32 @@ void Country::print() const
       << "Area: "       << area
       << "Density: "    << density;
 }
-/*
-void Country::set_population(double population)
-{
-   country_population = population;
-}
-void Country::set_area(double area)
-{
-   country_area = area;
-}
-*/
 int main()
 {
-   Country CountryA();
-   Country CountryB();
+   Country largest;
+   Country populous;
+   Country densest;
+
+   bool more = true;
+   while(more)
+   {
+      Country next;
+      next.read();
+      if (next.is_larger_than(largest))
+      largest = next;
+      if (next.is_more_populated_than(populous))
+      populous = next;
+      if (next.is_denser_than(densest))
+      densest = next;
+
+      cout << "Enter another country? (y/n) ";
+      string answer;
+      getline(cin, answer);
+      if (answer !="y")
+      more = false;
+   }
+   cout << "The largest country has a population of: ";
+   largest.print();
+
    return 0;
 }
