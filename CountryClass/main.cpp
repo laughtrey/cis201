260b2a4fbd25ff21144733ccbfbd4420509f1906
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-10-28 02:50:07 -0700

    still have to split this and add asserts, but done.

--- CountryClass/main.cpp
+++ CountryClass/main.cpp
@@ -21,19 +21,20 @@ private:
    string name;
 public:
    Country(); // Constructor
-   //Country(int area, int population); // Constructor with parameters
    void read(); // Mutator
    bool is_larger_than(Country b) const; // Compares the two countries and finds the larger area
    bool is_more_populated_than(Country next) const; // Compares the two countries and finds the more popuated
    bool is_denser_than(Country next) const; // Compares the two countries and finds the denser populated
-   void print() const; // Prints the results
+   void printpop() const; // Prints the results
+   void printarea() const; // Prints the results
+   void printdensity() const; // Prints the results
 };
 // Constructor
 Country::Country()
 {
    area = 1;
-   population = 0;
-   density = 0;
+   population = 1;
+   density = 1;
 }
 // ---Functions---
 // Mutator or setter functions
@@ -41,21 +42,22 @@ void Country::read()
 {
    cout << "Please enter the name of the country: ";
    getline(cin, name);
-   cout << "Please enter the population: ";
+   cout << "Please enter the population in millions: ";
    cin >> population;
-   cout << "Please enter the area in kilometers squared: ";
+   cout << "Please enter the area in kilometers squared in millions: ";
    cin >> area;
    density = (population / area);
    string remainder;
    getline(cin, remainder);
 }
+// Compares two areas
 bool Country::is_larger_than(Country b) const
 {
    if (area == 0) return true;
    if (b.area == 0) return false;
    return area > b.area;
 }
-
+// Compares two populations
 bool Country::is_more_populated_than(Country b) const
 {
    if (population == 0) return true;
@@ -63,6 +65,7 @@ bool Country::is_more_populated_than(Country b) const
    return population > b.population;
 
 }
+// Compares two densities
 bool Country::is_denser_than(Country b) const
 {
    if (density == 0) return true;
@@ -70,13 +73,23 @@ bool Country::is_denser_than(Country b) const
    return density > b.density;
 
 }
-
-void Country::print() const // split this into 3 for each type and call printlargest etc
+// Prints the population
+void Country::printpop() const
 {
    cout << name << endl;
    cout << "Population: " << population << endl;
+}
+// Prints the area
+void Country::printarea() const
+{
+   cout << name << endl;
    cout << "Area:" << area << endl;
-   cout << "Density: " << density << endl;
+}
+// Prints the density
+void Country::printdensity() const
+{
+   cout << name << endl;
+   cout << "Density: " << density << " million people per square kilometer."<< endl;
 }
 int main()
 {
@@ -90,7 +103,7 @@ int main()
       Country next;
       next.read();
       if (next.is_larger_than(largest))
-      largest = next; //this is erasing next. I need it to be able to keep checking the entered country.
+      largest = next;
       if (next.is_more_populated_than(populous))
       populous = next;
       if (next.is_denser_than(densest))
@@ -102,9 +115,9 @@ int main()
       if (answer != "y")
          more = false;
    }
-   cout << "The largest country by area is: "; largest.print();
-   cout << "The most populated country is: "; populous.print();
-   cout << "The most densely populated country is: "; densest.print();
+   cout << "The largest country by area is: "; largest.printarea();
+   cout << "The most populated country is: "; populous.printpop();
+   cout << "The most densely populated country is: "; densest.printdensity();
 
    return 0;
 }
