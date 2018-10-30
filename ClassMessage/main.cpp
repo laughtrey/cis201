119810dac5979d092ece9a7d824b014b86537250
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-10-30 16:15:53 -0700

    Fixed it so it's correct. Still need to add std:: to everything.

--- ClassMessage/main.cpp
+++ ClassMessage/main.cpp
@@ -9,13 +9,13 @@ using namespace std;
 class Message
 {
 private:
-   string recipient, sender, body, email, From, To, Message_body;
+   string m_recipient, m_sender, m_body, m_email;
    time_t timest;
 
 public:
    Message();
-   Message(string r, string s, string b); // Constructor that takes the sender and recipient and sets the time stamp to current time
-   void append(); // Appends a line of text to the message body
+   Message(string r, string s, time_t timest); // Constructor that takes the sender and recipient and sets the time stamp to current time
+   void append(string m); // Appends a line of text to the message body
    to_string(); // makes the message into one long string
    void print(); // prints the message text
 };
@@ -23,17 +23,17 @@ Message::Message()
 {
 
 }
-Message::Message(string r, string s, timest)
+Message::Message(string r, string s, time_t timest)
 {
-   recipient = r;
-   sender = s;
-   body = b;
+   m_recipient = r;
+   m_sender = s;
+   m_body = " ";
    timest = 0;
 
 }
 void Message::append(string m)
 {
-  m_message += '\n' + m;
+  m_body += '\n' + m;
   // concatenate the constructed recipient/sender with a message body string b
    /*cout << "Please enter your recipient: ";
    getline(cin, recipient);
@@ -46,17 +46,18 @@ void Message::append(string m)
 }
 Message::to_string() // Cocatinate the entire thing to one string email
 {
-   email = "From: " + sender + '\n' + "To: " + recipient + '\n' + "Sent at: " + ctime(&timest) + "Message: " + body;
+   m_email = "From: " + m_sender + '\n' + "To: " + m_recipient + '\n' + "Sent at: " + ctime(&timest) + "Message: " + m_body;
 
 }
 void Message::print() // just prints email.
 {
-   cout << email;
+   cout << m_email;
 }
 int main()
 {
-   Message mail("r","s", &timest);
-   mail.append("Appending message"); // Maybe this should be the constructor, the assignment was unclear.
+   time_t timest;
+   Message mail("Jon.Doe@Email.com","Jane.Doe@email.com", timest);
+   mail.append("Hey what's up!");
    mail.to_string();
    cout << "Your message is: "<< endl;
    mail.print();
