018eb1c381925e5471dc2fd0d480514fb9b95e88
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-11-02 03:29:43 -0700

    Cleaned and organized all these files in order of assignment.

---
+++ 8 Vectors/README.md
@@ -0,0 +1,8 @@
+# main.cpp
+
+File: main.cpp
+Description:
+Author: Raymond Laughrey
+Email: raymonl4963@student.vvc.edu
+Date of Creation:
+
---
+++ 8 Vectors/main.cpp
@@ -0,0 +1,13 @@
+/*
+File: main.cpp
+Description:
+Author: Raymond Laughrey
+Email: raymonl4963@student.vvc.edu
+Date of Creation:
+*/
+#include<iostream>
+
+int main()
+{
+   return 0;
+}
