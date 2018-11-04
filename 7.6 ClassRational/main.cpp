4e4e725c3885e9dfa9666a63ece1211acda3355d
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-11-03 19:14:11 -0700

    created and defined functions

---
+++ 7.6 ClassRational/main.cpp
@@ -0,0 +1,26 @@
+/*
+   File: main.cpp
+   Description:
+   Author: Raymond Laughrey
+   Email: raymonl4963@student.vvc.edu
+   Date of Creation:
+ */
+#include <iostream>
+
+class Rational
+{
+private:
+
+
+public:
+Rational (arguments);
+void Adding();
+void Subtraction();
+void Division();
+void Multiplication();
+void Print();
+};
+int main()
+{
+        return 0;
+}
