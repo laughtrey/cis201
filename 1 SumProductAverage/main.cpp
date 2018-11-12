9024b9b0517d31079e8b2ef3fe3ebf7e7f152807
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-11-12 01:38:47 -0800

    Un-Beautified it. Sorry that was annoying.

--- 1 SumProductAverage/main.cpp
+++ 1 SumProductAverage/main.cpp
@@ -1,46 +1,46 @@
 /*
-   file: main.cpp
-   Description: This program will ask the user for 3 numbers and will
-   calculate and print the sum the product and the average of those numbers.
-   Author: Raymond  Laughrey
-   Email: raymonl4963@student.vvc.edu
-   Date of Creation: 9/6/2018
- */
+file: main.cpp
+Description: This program will ask the user for 3 numbers and will
+calculate and print the sum the product and the average of those numbers.
+Author: Raymond  Laughrey
+Email: raymonl4963@student.vvc.edu
+Date of Creation: 9/6/2018
+*/
 
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
 int main()
 {
-								int sum,product,average,num1,num2,num3;
+	int sum,product,average,num1,num2,num3;
 
-								// Prompt for num1
-								cout << "Enter number 1: ";
-								cin >> num1;
+	// Prompt for num1
+	cout << "Enter number 1: ";
+	cin >> num1;
 
-								// Prompt for num2
-								cout << "Enter number 2: ";
-								cin >> num2;
+	// Prompt for num2
+	cout << "Enter number 2: ";
+	cin >> num2;
 
-								// Prompt for num3
-								cout << "Enter number 3: ";
-								cin >> num3;
+	// Prompt for num3
+	cout << "Enter number 3: ";
+	cin >> num3;
 
-								// Calculate the sum
-								sum = num1 + num2 + num3;
+	// Calculate the sum
+	sum = num1 + num2 + num3;
 
-								// Calculate the product
-								product = num1 * num2 * num3;
+	// Calculate the product
+	product = num1 * num2 * num3;
 
-								// Calculate the average
-								average = sum / 3;
+	// Calculate the average
+	average = sum / 3;
 
 
-								// Print the sum product and average
-								cout << "The sum is: " << sum << endl;
-								cout << "The product is: " << product << endl;
-								cout << "The average is: " << average << endl;
+	// Print the sum product and average
+	cout << "The sum is: " << sum << endl;
+	cout << "The product is: " << product << endl;
+	cout << "The average is: " << average << endl;
 
-								return 0;
+	return 0;
 }
